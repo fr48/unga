e59bf3f73d4d33c43241900edbe78373e7b9dcbd
fr48 <fr48@columbia.edu>
2019-12-03 14:26:37 -0500

    commenting Part 2

--- part2_economics.py
+++ part2_economics.py
@@ -2,7 +2,7 @@
 # Classifying the Digital Deciders:
 # The Choice Between an Open Internet and a Sovereign One
 # Fahmida Y Rashid (fr48)
-# Part 2: Comparing the corpus, cybersecurity
+# Part 2: Comparing the corpus, economics
 # # # # # # # # # #
 
 '''
@@ -24,7 +24,7 @@ from 1970 to 2018 to determine which worldview the Digital Deciders are more lik
 '''
 ## Part Two: Compare the corpus
 
-Now that I have figured out what speeches go in which corpus, I create the corpus and then compare them. 
+Cybersecurity didn't really reveal a lot of changes, so I am trying with economics now
 
 * Read in the relevant speeches (they have already been cleaned up)
 * Run TF/IDF on the corpus
@@ -75,11 +75,11 @@ sovereign_controlled = ['SAU', 'ZWE', 'VEN', 'SWZ', 'CUB', 'IRN', 'DZE', 'LBY',
                         'CMR', 'TKM', 'TJK', 'SYR', 'PRK', 'UZB', 'AGO', 'EGY']
 
 #################
-# Cybersecurity
+# Economics
 #################
 
 file_list = []
-file_name = "un/output/cs_deciders.csv"
+file_name = "un/output/ec_deciders.csv"
 with open(file_name) as f_input:
     file_list = f_input.read().split('\n')
 
@@ -91,72 +91,74 @@ for one_file in file_list:
         deciders.append(f_input.read())
 
 sovereigns = []
-file_name = "un/output/cs_sov_corpus.csv"
+file_name = "un/output/ec_sov_corpus.csv"
 with open(file_name) as f_input:
     sovereigns = f_input.read().split('\n')
 
 currents = []
-file_name = "un/output/cs_op_corpus.csv"
+file_name = "un/output/ec_op_corpus.csv"
 with open(file_name) as f_input:
     currents = f_input.read().split('\n')
 
 ## Open Internet TF/IDF
 
-#define the vectorizer, and the ngram range (one to three words)
-o_vectorizer = TfidfVectorizer(ngram_range=(1, 3))
+#define the vectorizer, and the ngram range (one to two words)
+#3-gram would be nice but I keep running out of memory
+eo_vectorizer = TfidfVectorizer(ngram_range=(1, 2))
 
 #fit the corpus to the vectorizer, and get the feature names
-o_vectors = o_vectorizer.fit_transform(currents)
-o_feature_names = o_vectorizer.get_feature_names()
-o_dense = o_vectors.todense()
-o_denselist = o_dense.tolist()
+eo_vectors = eo_vectorizer.fit_transform(currents)
+eo_feature_names = eo_vectorizer.get_feature_names()
+eo_dense = eo_vectors.todense()
+eo_denselist = eo_dense.tolist()
 
 #put the resulting calculations into a df and use feature_names for column names
-oc_df = pd.DataFrame(o_denselist, columns=o_feature_names)
+eo_df = pd.DataFrame(eo_denselist, columns=eo_feature_names)
 
 #use the same open vectorizers i used for the deciders
-covectors = o_vectorizer.transform(deciders)
-cofeature_names = o_vectorizer.get_feature_names()
-codense = covectors.todense()
-codenselist = codense.tolist()
+eo_vectors = eo_vectorizer.transform(deciders)
+eo_feature_names = eo_vectorizer.get_feature_names()
+eo_dense = eo_vectors.todense()
+eo_denselist = eo_dense.tolist()
 
 #deciders using the open vectorizer
-co_df = pd.DataFrame(codenselist, columns=cofeature_names, index=file_list)
+#eodf = pd.DataFrame(eo_denselist)
+eodf = pd.DataFrame(eo_denselist, columns=eo_feature_names, index=file_list)
 
 # there is a lot of 0.00 so just pull out a few specific features
 
 # compare deciders with open
-counts = cosine_similarity(oc_df,co_df)
-cs_open = pd.DataFrame(counts.T, index=file_list)
-cs_open=cs_open.rename(columns={0: "open"})
+counts = cosine_similarity(eo_df,eodf)
+ec_open = pd.DataFrame(counts.T, index=file_list)
+ec_open=ec_open.rename(columns={0: "open"})
 
 ## Sovereign Internet TF/IDF
 
 #define the vectorizer, and the ngram range (one to three words)
-s_vectorizer = TfidfVectorizer(ngram_range=(1, 3))
+es_vectorizer = TfidfVectorizer(ngram_range=(1, 2))
 
 #fit the corpus to the vectorizer, and get the feature names
-s_vectors = s_vectorizer.fit_transform(sovereigns)
-s_feature_names = s_vectorizer.get_feature_names()
-s_dense = s_vectors.todense()
-s_denselist = s_dense.tolist()
+es_vectors = es_vectorizer.fit_transform(sovereigns)
+es_feature_names = es_vectorizer.get_feature_names()
+es_dense = es_vectors.todense()
+es_denselist = es_dense.tolist()
 
 #put the resulting calculations into a df and use feature_names for column names
-sc_df = pd.DataFrame(s_denselist, columns=s_feature_names)
+es_df = pd.DataFrame(es_denselist, columns=es_feature_names)
 
 #use the same sovereign vectorizers i used
-csvectors = s_vectorizer.transform(deciders)
-csfeature_names = s_vectorizer.get_feature_names()
-csdense = csvectors.todense()
-csdenselist = csdense.tolist()
+es_vectors = es_vectorizer.transform(deciders)
+es_feature_names = es_vectorizer.get_feature_names()
+es_dense = es_vectors.todense()
+es_denselist = es_dense.tolist()
 
 #deciders using the sovereign vectorizer
-cs_df = pd.DataFrame(csdenselist, columns=csfeature_names, index=file_list)
+esdf = pd.DataFrame(es_denselist, columns=es_feature_names, index=file_list)
 
 # compare deciders with sovereign
-counts = cosine_similarity(sc_df,cs_df)
-cs_sovereign = pd.DataFrame(counts.T, index=file_list)
-cs_sovereign=cs_sovereign.rename(columns={0: "sovereign"})
+counts = cosine_similarity(es_df,esdf)
+ec_sovereign = pd.DataFrame(counts.T, index=file_list)
+ec_sovereign=ec_sovereign.rename(columns={0: "sovereign"})
 
 # compare in one table
-cs = pd.concat([cs_open, cs_sovereign], axis=1, sort=False)
+ec = pd.concat([ec_open, ec_sovereign], axis=1, sort=False)
